64e4dc4be08c98271702b0884d941bb38311c120
Priyanka <psharma0796@gmail.com>
2022-02-14 18:05:22 +0530

    first commit

---
+++ immigration.js
@@ -0,0 +1,19 @@
+function openSignUp(){
+  // alert('hello');
+  document.getElementById('signUp_form').style.display='flex';
+}
+function closeSignUp_btn(){
+  // alert('hello');
+  document.getElementById('signUp_form').style.display='none';
+}
+
+function openLogIn(){
+  document.getElementById('LogIn_form').style.display='flex';
+
+}
+
+function closeLogIn_btn(){
+  document.getElementById('LogIn_form').style.display='none';
+
+}
+      
